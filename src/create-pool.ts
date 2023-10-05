b68b5974763bf0fe49b11528a829790aed95635d
Matthew Watman <mattwatman@gmail.com>
2023-10-05 14:31:48 -0400

    add pool creation logic

--- src/create-pool.ts
+++ src/create-pool.ts
@@ -1,15 +1,15 @@
 import * as dotenv from "dotenv";
 
-import { ethers } from "ethers";
+import { ethers, Contract, ContractTransaction } from "ethers";
+import allo from "../abi/Allo.json";
 
 dotenv.config();
 
 async function main() {
- if (process.argv.length < 3) {
-  console.error("Please provide the CSV file path as an argument");
+ if (process.argv.length < 10) {
+  console.error("Please input at least 10 arguments");
   process.exit(1); // Exit the script with an error code
  }
- const filePath = process.argv[2];
 
  const provider = new ethers.providers.JsonRpcProvider(
   process.env.INFURA_RPC_URL as string,
@@ -18,4 +18,45 @@ async function main() {
   process.env.DEPLOYER_PRIVATE_KEY as string,
   provider,
  );
+ const alloContract: Contract = new ethers.Contract(
+  process.env.ALLO_ADDRESS as string,
+  allo.abi,
+  poolOwner,
+ );
+ const abiEncoder = ethers.utils.defaultAbiCoder;
+ const profileId = process.argv[2];
+ const strategyAddress = process.argv[3];
+ const initData = abiEncoder.encode(
+  ["uint64", "uint64", "uint256"],
+  [process.argv[4], process.argv[5], process.argv[6]],
+ );
+ const tokenAddress = process.argv[7];
+ const amount = process.argv[8];
+ const metadata = { protocol: 1, pointer: process.argv[9] };
+ const managers = process.argv.slice(9);
+
+ try {
+  const createTx: ContractTransaction =
+   await alloContract.createPoolWithCustomStrategy(
+    profileId,
+    strategyAddress,
+    initData,
+    tokenAddress,
+    amount,
+    metadata,
+    managers,
+   );
+  const txReceipt = await createTx.wait();
+  let logs = txReceipt?.logs.map((log: any) => {
+   return alloContract.interface.parseLog(log);
+  });
+  const poolLog = logs.find((log: any) => {
+   return log.name === "PoolCreated";
+  });
+  const { poolId } = poolLog.args;
+  console.info(`Created pool with ID ${poolId}`);
+ } catch (error) {
+  console.error(error);
+ }
 }
+main();
