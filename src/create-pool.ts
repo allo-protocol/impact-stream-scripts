007416bdfeea6bfce4872c714353eef58754eee5
Matthew Watman <mattwatman@gmail.com>
2023-10-20 16:53:04 -0400

    add create pool logic

--- src/create-pool.ts
+++ src/create-pool.ts
@@ -1,31 +1,38 @@
 import * as dotenv from "dotenv";
+import { parse } from "csv";
+import { finished } from "stream/promises";
+import fs from "fs";
 
+import { PoolDeployment } from "../types";
 import {
  alloContract,
  abiEncoder,
+ ethers,
  ContractTransaction,
 } from "../common/ethers";
 
 dotenv.config();
 
 async function main() {
- if (process.argv.length < 10) {
-  console.error("Please input at least 10 arguments");
+ if (process.argv.length < 2) {
+  console.error("Please provide the CSV file path as an argument");
   process.exit(1); // Exit the script with an error code
  }
-
- const profileId = process.argv[2];
- const strategyAddress = process.argv[3];
- const initData = abiEncoder.encode(
-  ["uint64", "uint64", "uint256"],
-  [process.argv[4], process.argv[5], process.argv[6]],
- );
- const tokenAddress = process.argv[7];
- const amount = process.argv[8];
- const metadata = { protocol: 1, pointer: process.argv[9] };
- const managers = process.argv.slice(9);
+ const filePath = process.argv[2];
+ const deploymentData = await processFile(filePath);
+ console.log(deploymentData);
+ const {
+  profileId,
+  strategyAddress,
+  initData,
+  tokenAddress,
+  amount,
+  metadata,
+  managers,
+ } = deploymentData[0];
 
  try {
+  console.info("Creating pool...");
   const createTx: ContractTransaction =
    await alloContract.createPoolWithCustomStrategy(
     profileId,
@@ -35,6 +42,9 @@ async function main() {
     amount,
     metadata,
     managers,
+    {
+     gasLimit: 10000000,
+    },
    );
   const txReceipt = await createTx.wait();
   let logs = txReceipt?.logs.map((log: any) => {
@@ -49,4 +59,36 @@ async function main() {
   console.error(error);
  }
 }
+const processFile = async (filePath: string): Promise<PoolDeployment[]> => {
+ let records: string[][] = [];
+ const parser = fs.createReadStream(filePath).pipe(
+  parse({
+   delimiter: ",",
+   bom: true,
+  }),
+ );
+ parser.on("readable", function () {
+  let record;
+  while ((record = parser.read()) !== null) {
+   // Work with each record
+   records.push(record);
+  }
+ });
+ await finished(parser);
+ const deploymentObjects: any[] = records.map((record) => {
+  return {
+   profileId: record[0],
+   strategyAddress: record[1],
+   initData: abiEncoder.encode(
+    ["tuple(uint64, uint64, uint256)"],
+    [[record[2], record[3], record[4]]],
+   ),
+   tokenAddress: record[5],
+   amount: record[6],
+   metadata: { protocol: 1, pointer: record[7] },
+   managers: record[8].split(","),
+  };
+ });
+ return deploymentObjects;
+};
 main();
