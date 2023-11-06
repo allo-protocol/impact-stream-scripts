517f8d9564bed8aa5be97994291c8b9fb755af9b
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-06 19:33:30 +0530

    fix script

--- src/create-recipients.ts
+++ src/create-recipients.ts
@@ -6,18 +6,36 @@ import fsPromises from "fs/promises";
 
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-import { abiEncoder, alloContract } from "../common/ethers";
 import { storage, createFileObject } from "../common/ipfs";
 import { RawSupabaseData, Recipient } from "../types";
 import { Contract, ethers } from "ethers";
+import allo from "../abi/Allo.json";
 
 dotenv.config();
 
+const EMPTY_METADATA = [0, ""];
+const EMPTY_RECIPIENT_ID = "0x0000000000000000000000000000000000000000";
+
 async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.INFURA_RPC_URL as string
+  );
+
+  const signer = new ethers.Wallet(
+    process.env.SIGNER_PRIVATE_KEY as string,
+    provider
+  );
+  const alloContract: Contract = new ethers.Contract(
+    process.env.ALLO_MAIN_ADDRESS as string,
+    allo.abi,
+    signer
+  );
+
   if (process.argv.length < 3) {
     console.error("Please provide the CSV file path as an argument");
-    process.exit(1); // Exit the script with an error code
+    process.exit(1);
   }
+
   const filePath = process.argv[2];
   const supabaseData = await processFile(filePath);
 
@@ -28,12 +46,11 @@ async function main() {
       auth: {
         persistSession: false,
       },
-    },
+    }
   );
 
   const recipients = await Promise.all(
     supabaseData.map(async (recipient: RawSupabaseData) => {
-      let cid: string;
       try {
         const fileObject = createFileObject({
           name: `${recipient.allo_recipient_id}`,
@@ -43,110 +60,138 @@ async function main() {
           },
         });
 
-        cid = await storage.put([fileObject], { wrapWithDirectory: false });
+        await storage.put([fileObject], { wrapWithDirectory: false });
       } catch (error) {
-        cid = "";
         console.error(error);
       }
       return {
+        proposalId: recipient.proposal_id as string,
         userId: recipient.author.id as string,
-        recipientId: recipient.allo_recipient_id as string,
         recipientAddress: recipient.safe_address as string,
-        requestedAmount: recipient.minimum_budget as number,
-        metadata: {
-          protocol: 1,
-          pointer: cid ? `https://ipfs.w3s.link/${cid}` : "",
-        },
+        requestedAmount: recipient.minimum_budget as number
       };
-    }),
+    })
   );
 
-  // todo alloContract
   await createRecipients(recipients, alloContract, supabaseAdmin);
 }
 
 const processFile = async (filePath: string): Promise<RawSupabaseData[]> => {
- let records: string[][] = [];
- const parser = fs.createReadStream(filePath).pipe(
-  parse({
-   delimiter: ",",
-   bom: true,
-  }),
- );
- parser.on("readable", function () {
-  let record;
-  while ((record = parser.read()) !== null) {
-   // Work with each record
-   records.push(record);
-  }
- });
- await finished(parser);
- const recipients: any[] = records
-  .slice(1) // Remove the header
-  .map((record) => {
-   return {
-     proposal_id: record[0],
-     author: JSON.parse(record[1]),
-     collaborators: record[2].length === 0 ? null : JSON.parse(record[2]),
-     mimimum_budget: parseInt(record[3]),
-     // recipient_id: record[4],
-     recipient_id: "0x0000000000000000000000000000000000000000",
-     safe_address: record[5],
-   };
+  let records: string[][] = [];
+
+  const parser = fs.createReadStream(filePath).pipe(
+    parse({
+      delimiter: ",",
+      bom: true,
+    })
+  );
+
+  parser.on("readable", function () {
+    let record;
+    while ((record = parser.read()) !== null) {
+      records.push(record);
+    }
   });
- return recipients;
+
+  await finished(parser);
+
+  const recipients: any[] = records
+    .slice(1) // Remove the header
+    .map((record) => {
+      return {
+        proposal_id: record[0],
+        author: JSON.parse(record[1]),
+        collaborators: record[2].length === 0 ? null : JSON.parse(record[2]),
+        mimimum_budget: parseInt(record[3]),
+        // recipient_id: record[4],
+        // recipient_id: "0x0000000000000000000000000000000000000000",
+        safe_address: record[5],
+      };
+    });
+  return recipients;
 };
 
 const createRecipients = async (
-  recipientList: Recipient[],
-  alloContract: Contract, // todo Contract
-  supabaseClient: SupabaseClient,
+  recipients: Recipient[],
+  alloContract: Contract,
+  supabaseClient: SupabaseClient
 ) => {
   let resultsData: any[] = [
     "\ufeff", // BOM
     "id,recipient_created,recipient_id,supabase_updated\n", // Header
   ];
-  for (const recipient of recipientList) {
+
+  for (const recipient of recipients) {
     const userId = recipient.userId;
-    let resultDataRow = `${userId},`;
+
+    let resultDataRow = `${recipient.proposalId},`;
+    
     console.log("=======================");
-    console.info(`Creating Allo recipient for ${userId}...`);
+    console.info(`Creating Allo recipient for ${recipient.proposalId}...`);
 
     try {
-      const { recipientId, recipientAddress, requestedAmount, metadata } =
-        recipient;
+
+      const recipientAddress = recipient.recipientAddress;
+      const requestedAmount = recipient.requestedAmount;
+      
       const recipientRegisterData = ethers.utils.defaultAbiCoder.encode(
-        ["address", "address", "uint256", "Metadata"],
-        [recipientId, recipientAddress, requestedAmount, metadata],
+        ["address", "address", "uint256", "tuple(uint256, string)"],
+        [EMPTY_RECIPIENT_ID, recipientAddress, requestedAmount, EMPTY_METADATA]
       );
+
+      const staticCallResult = await alloContract.registerRecipient(
+        process.env.ALLO_POOL_ID,
+        recipientRegisterData
+      );
+
       const createTx = await alloContract.registerRecipient(
         process.env.ALLO_POOL_ID,
-        recipientRegisterData,
+        recipientRegisterData
       );
-      const txReceipt = await createTx.wait();
-      resultDataRow += `true,${recipientId},`;
+  
+      await createTx.wait();
+
+      // get recipientId after registering
+      const recipientId = staticCallResult.toString();
+
+      resultDataRow += `${userId},${recipientId},`;
       console.info(`Allo recipient created with id ${recipientId}`);
+      
       try {
+
+        // TODO: check if this is needed
+        // const { error } = await supabaseClient
+        //   .from("users")
+        //   .update({ allo_recipient_id: recipientId })
+        //   .eq("author_id", userId);
+
+        // make sure proposal is linked to recipientId
+
         const { error } = await supabaseClient
-          .from("users")
+          .from("proposals")
           .update({ allo_recipient_id: recipientId })
-          .eq("author_id", userId);
+          .eq("id", recipient.proposalId);
+        
         if (error) throw error;
+
         resultDataRow += `true\n`;
-        console.info(`Allo recipient id ${recipientId} added to supabase`);
+        console.info(`Allo recipient id ${recipientId} - ${recipient.proposalId} added to supabase`);
         console.log("=======================");
       } catch (error) {
+        
         resultDataRow += `false\n`;
         console.error(error);
         console.log("=======================");
       }
     } catch (error) {
-      resultDataRow += `false,,false\n`;
+      resultDataRow += `false\n`;
       console.error(error);
     }
     resultsData.push(resultDataRow);
   }
+
   try {
+    // write userId -> proposalId -> recipientId -> link status to csv 
     await fsPromises.writeFile("recipients.csv", resultsData.join(""));
   } catch (error) {
     console.error(error);
--- types.ts
+++ types.ts
@@ -1,67 +1,70 @@
 export type User = {
- id: string;
- address: string;
+  id: string;
+  address: string;
 };
 
 export type Metadata = {
- protocol: number;
- pointer: string;
+  protocol: number;
+  pointer: string;
 };
 
 export type Profile = {
- userId: string;
- nonce: number;
- name: string;
- metadata: Metadata;
- owner: string;
- members: string[];
+  userId: string;
+  nonce: number;
+  name: string;
+  metadata: Metadata;
+  owner: string;
+  members: string[];
 };
 
 export type Recipient = {
- userId: string;
- recipientId: string;
- recipientAddress: string;
- requestedAmount: number;
- metadata: Metadata;
+  proposalId: string;
+  userId: string;
+  recipientAddress: string; // safe address
+  requestedAmount: number;
 };
 
 export type RawSupabaseData = {
- proposal_id: string;
- author: {
-  id: string;
-  name?: string;
-  family_name?: string;
-  address?: string;
- };
- collaborators?: string[];
- minimum_budget?: number;
- allo_recipient_id?: string;
- safe_address?: string;
+  proposal_id: string;
+  author: {
+    id: string;
+    name?: string;
+    family_name?: string;
+    address?: string;
+  };
+  collaborators?: string[];
+  minimum_budget?: number;
+  allo_recipient_id?: string;
+  safe_address?: string;
 };
 
 export type PoolDeployment = {
- profileId: string;
- strategyAddress: string;
- initData: string;
- tokenAddress: string;
- amount: string;
- metadata: Metadata;
- managers: string[];
+  profileId: string;
+  strategyAddress: string;
+  initData: string;
+  tokenAddress: string;
+  amount: string;
+  metadata: Metadata;
+  managers: string[];
 };
+
 export type DistributionList = string[];
+
 export type AddressList = string[];
+
 export type Payout = {
- recipientId: string;
- amount: number;
+  recipientId: string;
+  amount: number;
 };
 
 export type AllocationEvent = {
- recipientId: string;
- voteResult: number;
+  recipientId: string;
+  voteResult: number;
 };
+
 export type RawFileData = {
- name: string;
- data: {
-  [key: string]: string;
- };
+  name: string;
+  data: {
+    [key: string]: string;
+  };
 };
