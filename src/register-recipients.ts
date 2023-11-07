2dce2a2cb388242336bc685f5fc88b1533ebbf49
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-07 10:59:51 +0530

    fix register recipient

--- src/register-recipients.ts
+++ src/register-recipients.ts
@@ -12,11 +12,16 @@ import { Recipient } from "../types";
 dotenv.config();
 
 const EMPTY_METADATA = [0, ""];
-const EMPTY_RECIPIENT_ID = "0x0000000000000000000000000000000000000000";
 
 async function registerRecipient() {
   const usersWithSafe = await getUsersWithSafe();
-  const unregisteredApprovedProposals = await getUnregisteredApprovedProposals();
+  const unregisteredApprovedProposals =
+    await getUnregisteredApprovedProposals();
+
+  console.log(
+    "Unregistered Approved Proposals: ",
+    unregisteredApprovedProposals.data!.length
+  );
 
   let recipients: any = [];
   let recipientRegisterData: any = [];
@@ -29,8 +34,6 @@ async function registerRecipient() {
     );
 
     if (proposalsByUser.length > 0) {
-      console.info(`User ${user.id} has approved proposals`);
-
       for (const proposal of proposalsByUser) {
         const recipient = {
           proposalId: proposal.id,
@@ -42,6 +45,15 @@ async function registerRecipient() {
         const onChainRecipient = await strategyContract.callStatic.getRecipient(
           proposal.allo_recipient_id
         );
+
+        if (onChainRecipient.recipientStatus != 0) {
+          console.log(
+            "Skipping as Proposal already registered: ",
+            proposal.id,
+            "with recipientId: ",
+            proposal.allo_recipient_id
+          );
+        }
 
         if (onChainRecipient.recipientStatus === 0) {
           recipientRegisterData.push(
@@ -58,17 +70,22 @@ async function registerRecipient() {
           poolIds.push(Number(process.env.ALLO_POOL_ID));
           console.log("Recipients to be created: ", recipients.length);
         }
-      }
 
-      console.log("Creating Recipients ...");
+        recipients.push(recipient);
+      }
     }
   }
 
-  await createRecipients(recipients, alloContract, supabaseAdmin);
-  await registerOnchain(poolIds, recipientRegisterData, alloContract);
+  if (recipients.length === 0) return;
+
+  if (recipientRegisterData.length > 0) {
+    // Bulk Register Recipients
+    await batchRegisterRecipients(poolIds, recipientRegisterData, alloContract);
+  }
+  await markRecipientsAsRegistered(recipients, supabaseAdmin);
 }
 
-const registerOnchain = async (
+const batchRegisterRecipients = async (
   poolIds: number[],
   recipientRegisterData: any[],
   alloContract: any
@@ -99,61 +116,23 @@ const registerOnchain = async (
   }
 };
 
-const createRecipients = async (
+const markRecipientsAsRegistered = async (
   recipients: Recipient[],
-  alloContract: Contract,
   supabaseClient: SupabaseClient
 ) => {
   for (const recipient of recipients) {
-    console.info(`Creating Allo recipient for ${recipient.proposalId}...`);
-
     try {
-      const recipientAddress = recipient.recipientAddress;
-      const requestedAmount = recipient.requestedAmount;
+      // update the registered and funded flag in the database
+      const { error: updateError } = await supabaseClient
+        .from("proposals")
+        .update({ registered: true, funded: false })
+        .eq("author_id", recipient.userId)
+        .neq("allo_recipient_id", null);
 
-      const recipientRegisterData = ethers.utils.defaultAbiCoder.encode(
-        ["address", "address", "uint256", "tuple(uint256, string)"],
-        [EMPTY_RECIPIENT_ID, recipientAddress, requestedAmount, EMPTY_METADATA]
-      );
-
-      const staticCallResult = await alloContract.registerRecipient(
-        process.env.ALLO_POOL_ID,
-        recipientRegisterData
-      );
-
-      const createTx = await alloContract.registerRecipient(
-        process.env.ALLO_POOL_ID,
-        recipientRegisterData
-      );
-
-      await createTx.wait();
-
-      // get recipientId after registering
-      const recipientId = staticCallResult.toString();
-
-      console.info(`Allo recipient created with id ${recipientId}`);
-
-      try {
-        // update the registered and funded flag in the database
-        const { error: updateError } = await supabaseClient
-          .from("proposals")
-          .update({ registered: true, funded: false })
-          .eq("author_id", recipient.userId);
-
-        if (updateError) throw updateError;
-
-        const { error } = await supabaseClient
-          .from("proposals")
-          .update({ allo_recipient_id: recipientId })
-          .eq("id", recipient.proposalId);
-
-        if (error) throw error;
-      } catch (error) {
-        console.error(`DB Update Failure. UserId: ${recipient.proposalId}`);
-        console.error(error);
-      }
+      if (updateError) throw updateError;
+      console.log("Marked proposal", recipient.proposalId, "as registered");
     } catch (error) {
-      console.info(`Register Failure: UserId: ${recipient.proposalId}`);
+      console.error(`DB Update Failure. UserId: ${recipient.proposalId}`);
       console.error(error);
     }
   }
